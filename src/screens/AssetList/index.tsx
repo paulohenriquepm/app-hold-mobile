1cdb00fbac40a0f3e31d06372830541f77fc5f59
paulohenriquepm <paulo.henriquepm@outlook.com>
2021-12-09 21:18:06 -0300

    feat: add AssetList screen style

--- src/screens/AssetList/index.tsx
+++ src/screens/AssetList/index.tsx
@@ -1,11 +1,105 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+
+import { Title } from '../../components/atoms/Title';
+import { ThemeSwitcher } from '../../components/molecules/ThemeSwitcher';
+
+import {
+  Container,
+  Content,
+  Header,
+  AssetListContainer,
+  Asset,
+  AssetInfo,
+  AssetName,
+  AssetSector,
+  AssetLogo,
+} from './styles';
 
 const AssetList = () => {
   return (
-    <View>
-      <Text>AssetList</Text>
-    </View>
+    <Container>
+      <ThemeSwitcher />
+
+      <Content>
+        <Header>
+          <Title>Ativos</Title>
+        </Header>
+
+        <AssetListContainer>
+          <Asset>
+            <AssetInfo>
+              <AssetName>WEG - WEGE3</AssetName>
+              <AssetSector>Motores e Compressores</AssetSector>
+            </AssetInfo>
+            <AssetLogo source={{ uri: 'https://i.imgur.com/GpW6oKN.png' }} />
+          </Asset>
+
+          <Asset>
+            <AssetInfo>
+              <AssetName>WEG - WEGE3</AssetName>
+              <AssetSector>Motores e Compressores</AssetSector>
+            </AssetInfo>
+            <AssetLogo source={{ uri: 'https://i.imgur.com/GpW6oKN.png' }} />
+          </Asset>
+          <Asset>
+            <AssetInfo>
+              <AssetName>WEG - WEGE3</AssetName>
+              <AssetSector>Motores e Compressores</AssetSector>
+            </AssetInfo>
+            <AssetLogo source={{ uri: 'https://i.imgur.com/GpW6oKN.png' }} />
+          </Asset>
+          <Asset>
+            <AssetInfo>
+              <AssetName>WEG - WEGE3</AssetName>
+              <AssetSector>Motores e Compressores</AssetSector>
+            </AssetInfo>
+            <AssetLogo source={{ uri: 'https://i.imgur.com/GpW6oKN.png' }} />
+          </Asset>
+          <Asset>
+            <AssetInfo>
+              <AssetName>WEG - WEGE3</AssetName>
+              <AssetSector>Motores e Compressores</AssetSector>
+            </AssetInfo>
+            <AssetLogo source={{ uri: 'https://i.imgur.com/GpW6oKN.png' }} />
+          </Asset>
+          <Asset>
+            <AssetInfo>
+              <AssetName>WEG - WEGE3</AssetName>
+              <AssetSector>Motores e Compressores</AssetSector>
+            </AssetInfo>
+            <AssetLogo source={{ uri: 'https://i.imgur.com/GpW6oKN.png' }} />
+          </Asset>
+          <Asset>
+            <AssetInfo>
+              <AssetName>WEG - WEGE3</AssetName>
+              <AssetSector>Motores e Compressores</AssetSector>
+            </AssetInfo>
+            <AssetLogo source={{ uri: 'https://i.imgur.com/GpW6oKN.png' }} />
+          </Asset>
+          <Asset>
+            <AssetInfo>
+              <AssetName>WEG - WEGE3</AssetName>
+              <AssetSector>Motores e Compressores</AssetSector>
+            </AssetInfo>
+            <AssetLogo source={{ uri: 'https://i.imgur.com/GpW6oKN.png' }} />
+          </Asset>
+          <Asset>
+            <AssetInfo>
+              <AssetName>WEG - WEGE3</AssetName>
+              <AssetSector>Motores e Compressores</AssetSector>
+            </AssetInfo>
+            <AssetLogo source={{ uri: 'https://i.imgur.com/GpW6oKN.png' }} />
+          </Asset>
+          <Asset>
+            <AssetInfo>
+              <AssetName>WEG - WEGE3</AssetName>
+              <AssetSector>Motores e Compressores</AssetSector>
+            </AssetInfo>
+            <AssetLogo source={{ uri: 'https://i.imgur.com/GpW6oKN.png' }} />
+          </Asset>
+        </AssetListContainer>
+      </Content>
+    </Container>
   );
 };
 
