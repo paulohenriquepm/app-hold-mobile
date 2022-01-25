e2c4732f7dc2a972d35622309edbc6099f840261
paulohenriquepm <paulo.henriquepm@outlook.com>
2022-01-25 12:37:15 -0300

    fix: set prod env

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: `https://app-and-hold.herokuapp.com`,
-  baseURL: `http://192.168.15.6:3333`,
+  baseURL: `https://app-and-hold.herokuapp.com`,
 });
 
 export { api };
