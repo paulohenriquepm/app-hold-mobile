feb6c18f8022c2e3bb4f5144cf1fd0263220c441
paulohenriquepm <paulo.henriquepm@outlook.com>
2022-01-26 20:57:34 -0300

    feat: setup eas build

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,13 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig(__dirname);
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-    },
-  };
-})();
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.transformer.babelTransformerPath = require.resolve(
+  'react-native-svg-transformer',
+);
+defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter(
+  ext => ext !== 'svg',
+);
+defaultConfig.resolver.sourceExts.push('svg');
+
+module.exports = defaultConfig;
